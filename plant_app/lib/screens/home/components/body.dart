6cdc6f0d2ea02dbdcfb6711ce953086486b3d779
= <=>
2021-03-15 08:24:36 -0300

    update body.dart

--- plant_app/lib/screens/home/components/body.dart
+++ plant_app/lib/screens/home/components/body.dart
@@ -3,6 +3,9 @@ import 'package:plant_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'header_with_searchbox.dart';
+import 'title_with_more_btn.dart';
+
 class Body extends StatelessWidget {
 
   @override
@@ -13,92 +16,36 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
-        ],
-      ),
-    );
-  }
-}
-
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
-    Key key,
-    @required this.size,
-  }) : super(key: key);
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: size.height * 0.2,
-      child: Stack(
-        children: <Widget>[
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
           Container(
-            padding: EdgeInsets.only(
+            margin: EdgeInsets.only(
               left: kDefaultPadding,
-              right: kDefaultPadding,
-              bottom: 36 + kDefaultPadding,
+              top: kDefaultPadding / 2,
+              bottom: kDefaultPadding * 2.5
             ),
-            height: size.height * 0.2 - 27,
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(36),
-                bottomRight: Radius.circular(36),
-              ),
-            ),
-            child: Row(
-              children: <Widget>[
-                Text('Hi Uishopy!', style: Theme.of(context).textTheme.headline5.copyWith(
-                  color: Colors.white, fontWeight: FontWeight.bold)
-                ),
-                Spacer(),
-                Image.asset("assets/images/logo.png"),
-              ] 
-            )
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 54,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color:kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget> [
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        hintStyle: TextStyle(color: kPrimaryColor.withOpacity(0.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        //suffixIcon: SvgPicture.asset("assets/icons/search.svg")
+            width: size.width * 0.4,
+            child: Column(
+              children: <Widget> [
+                Image.asset("assets/images/image_1.png"),
+                Container(
+                  padding: EdgeInsets.all(kDefaultPadding / 2),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
                       )
-                    ),
-                  ),
-                  SvgPicture.asset("assets/icons/search.svg"),
-                ],
-              ),
-            ),
-          ),
+                    ]
+                  )
+                )
+              ],
+            )
+          )
         ],
       ),
     );
   }
 }
+
